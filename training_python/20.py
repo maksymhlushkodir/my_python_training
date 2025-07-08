6c74585f7a811fe0c9dbd68bfe8ac79b87597ca4
maksymhlushkodir <maksymhlushko725@gmail.com>
2025-07-08 18:24:39 +0300

    open file.txt r and a, matplotlib as plt

---
+++ training_python/20.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+from setuptools.command.rotate import rotate
+
+games = []
+ratings = []
+
+with open('games.txt', 'r', encoding='utf-8') as file_r:
+    for line in file_r:
+        line = line.strip()
+        if line:
+            parts = line.split()
+            game_name = ''.join(parts[:-1])
+            rating = int(parts[-1])
+            games.append(game_name)
+            ratings.append(rating)
+
+
+while True:
+    user_choice = input("1.add game / 2.further")
+
+    if user_choice == '1':
+        add_game = input("game-name and game-estimates")
+        with open('games.txt', 'a', encoding='utf-8') as file_a:
+            file_a.write("\n" + add_game + "\n")
+            games.append(add_game)
+
+    elif user_choice == '2':
+        break
+
+    else:
+        print("Error - you have uploaded data that cannot be disassembled")
+
+colors = ['green' if good >=0 else 'red' for good in ratings]
+
+plt.bar(games, ratings, color=colors)
+plt.xlabel('game name')
+plt.ylabel('rating')
+plt.title('rating game')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+
+
